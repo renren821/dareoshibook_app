6ecfcc48dab1ce22fc5d5bc25d3bbac1cd09f374
ren <yk.r.x.821@gmail.com>
2020-10-03 22:46:51 +0900

    Homeページのテスト

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
   gem 'byebug',  '9.0.6', platform: :mri
   gem "rspec-rails"
   gem "factory_bot_rails"
+  gem 'capybara', '~> 2.13'
 end
 
 group :development do
--- spec/features/static_page_spec.rb
+++ spec/features/static_page_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "StaticPages", type: :feature do
-  
+  describe "Home page" do
+    before do
+      visit root_path   # 名前付きルートを使用
+    end
+
+    it "Homeページに誰推しBookと表示されていること" do
+      expect(page).to have_content "誰推しBook"
+    end
+
+    it "タイトルが正しく表示されていること" do
+      expect(page).to have_title full_title('')
+    end
+  end
 end
